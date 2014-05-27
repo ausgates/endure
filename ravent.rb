5ee9244d5e762b2a6c881b27a2b51c66c8828882
Josh <joshception@icloud.com>
2014-05-27 15:17:17 -0700

    more stuff

--- ravent.rb
+++ ravent.rb
@@ -12,6 +12,7 @@
 VERSION = 0.1
 input ARGV[0]
 require 'rainbow'
+difficulty = nil
 
 def exit
   Process.exit(0)
@@ -20,4 +21,16 @@ end
 if input.nil?
   puts 'Arguments are required'
   exit
+end
+
+# Main Stuff
+case input
+when 'easy'
+  difficulty = 'easy'
+when 'medium'
+  difficulty = 'medium'
+when 'hard'
+  difficulty = 'hard'
+else
+  puts 'What?'
 end
