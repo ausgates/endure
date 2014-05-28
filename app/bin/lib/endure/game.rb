7fee5677db162a109f331ef3affd4f068d6af16d
josh <joshception@icloud.com>
2014-05-27 23:13:52 -0700

    converting to health instead of lives

--- app/bin/lib/endure/game.rb
+++ app/bin/lib/endure/game.rb
@@ -11,11 +11,11 @@ def game(name, difficulty)
     lives = 3
   end
 
-  player = Player.new(name, lives)
-  player_name = Rainbow(player.name).green
-  player_lives = Rainbow(player.lives).blue
+  player = Player.new(name, health)
+  player_name = Rainbow(player.health).green
+  player_health = Rainbow(player.health).blue
 
   # Actual game
   puts "New player #{player_name} initialized"
-  puts "Player #{player_name} has #{player_lives} lives"
+  puts "Player #{player_name} has #{player_health}% health"
 end
