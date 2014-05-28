cf3f6f4c15824f49d99b5f4a92fc0276718db5b6
josh <joshception@icloud.com>
2014-05-28 01:03:50 -0700

    ok cool

--- app/bin/lib/endure/game.rb
+++ app/bin/lib/endure/game.rb
@@ -11,11 +11,15 @@ def game(name, difficulty)
     health = 50
   end
 
+  # Do some instantiating
   player = Player.new(name, health)
+
+  # Create 'player_name' and 'player_health' variables,
+  # which contain the appropriately coloured versions of themselves
   player_name = Rainbow(player.name).green
   player_health = Rainbow(player.health).blue
 
-  # Actual game
+  # The actual game
   puts "New player #{player_name} initialized"
   puts "Player #{player_name} has #{player_health}% health"
 end
