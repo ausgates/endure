3d099ab87a08f115b225e9c5bc3180afe0d62462
josh <joshception@icloud.com>
2014-05-31 21:19:24 -0700

    ok

--- app/bin/lib/endure/player.rb
+++ app/bin/lib/endure/player.rb
@@ -147,7 +147,7 @@ class Player
     elsif @items.include? thing
       puts Rainbow("Are you sure you want to trash your #{thing}? [yes/no]").red
       if thing == 'lint' || STDIN.gets.chomp == 'yes'
-        puts Rainbow("#{player_name} couldn't get the lint out of his pocket. It's stuck.")
+        puts Rainbow("#{Rainbow(@player).green} couldn't get the lint out of his pocket. It's stuck.")
         pause
       else
         @items.delete_at(@items.index(thing)) if STDIN.gets.chomp.downcase == 'yes'
